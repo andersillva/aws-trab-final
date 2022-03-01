318a7f0278406126d7d5a2135c6a8ca0bfa7ba12
Anderson Machado da Silva <silva.andersonm@gmail.com>
2022-03-01 16:57:44 -0300

    Ajuste do nome do pacote.

--- src/test/java/br/com/silvaandersonm/trabfinal/ExemploApplicationTests.java
+++ src/test/java/br/com/silvaandersonm/trabfinal/ExemploApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.silvaandersonm.exemplo;
+package br.com.silvaandersonm.trabfinal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ExemploApplicationTests {
+class TrabFinalApplicationTests {
 
 	@Test
 	void contextLoads() {
